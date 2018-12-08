1067268bde6421da7b54a705506d97242d19dfde
Geoffrey Emerson <geoffrey.emerson.git@gmail.com>
2018-12-08 12:35:39 -0800

    refactor and add delete post method

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,37 +1,21 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    state = {
-        loadedPost: null,
-        postAuthor: null,
-    }
-
-    async componentDidUpdate() {
-        if (this.props.id && (!this.state.loadedPost || this.props.id !== this.state.loadedPost.id)) {
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`);
-            this.setState({loadedPost: response.data });
-            if (response.data.userId) {
-                const response2 = await axios.get(`https://jsonplaceholder.typicode.com/users/${response.data.userId}`);
-                this.setState({postAuthor: response2.data});
-            }
-        }
-    }
     render () {
         let post = <p style={{textAlign:'center'}}>Please select a Post!</p>;
         if (this.props.id) {
             post = <p style={{textAlign:'center'}}>Loading!</p>;
         }
-        if (this.props.id && this.state.loadedPost && this.state.postAuthor) {
+        if (this.props.id) {
             post = (
                 <div className="FullPost">
-                    <h1>{this.state.loadedPost.title}</h1>
-                    <p>By {this.state.postAuthor.name}</p>
-                    <p>{this.state.loadedPost.body}</p>
+                    <h1>{this.props.title}</h1>
+                    <p>By {this.props.author}</p>
+                    <p>{this.props.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.props.deletePost} className="Delete">Delete</button>
                     </div>
                 </div>
             );
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,42 +8,78 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: [],
-        selectedPostId: null
+        postArray: [],
+        postData: {},
+        authors: {},
+        selectedPostId: null,
+        error: false
     }
 
     async componentDidMount() {
         try {
             const response = await axios.get('http://jsonplaceholder.typicode.com/posts');
-            const posts = response.data.slice(0, 4);
-            const updatedPosts = posts.map(post => {
-                return {
-                    ...post,
-                    author: 'Max'
-                };
+            const postData = {};
+            response.data.forEach(post => {
+                postData[post.id] = post;
             });
-            this.setState({posts: updatedPosts});
+            const postArray = response.data; //.slice(0, 4);
+            console.log('Getting postArray: ', postArray);
+            const authors = {};
+            postArray.forEach(post => {
+                if(!authors[post.userId]) {
+                    authors[post.userId] = {name:'Loading author info...'};
+                    this.loadUser(post.userId);
+                }
+            });
+            this.setState({postArray, postData, authors});
         } catch(e) {
             console.log(`Axios error: ${e}`);
+            this.setState({ error: true });
         }
+    }
+
+    loadUser = async (userId) => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+        const authors = this.state.authors;
+        authors[userId] = response.data;
+        console.log(`loaded data for user:${userId}`, authors[userId]);
+        this.setState({authors});
     }
 
     postSelectedHandler = (id) => {
         this.setState({selectedPostId:id});
     }
 
-    render () {
-        console.log('Rendering Blog.js:');
-        console.log(this.state.posts);
+    deletePostHandler = async (postId) => {
+        const response = await axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+        console.log('Deleted: ', response);
+    }
 
-        const posts = this.state.posts.map(post => (
-            <Post
-                title={post.title}
-                author={post.author}
-                key={post.id}
-                clicked={() => this.postSelectedHandler(post.id)}
-            />
-        ));
+    render () {
+        let posts = <p style={{textAlign:'center'}}>Loading...</p>;
+        let selectedPostId, selectedPost, author, title, body;
+
+        if (!this.state.error) {
+            posts = this.state.postArray.slice(0,4).map(post => {
+                return (
+                    <Post
+                        title={post.title}
+                        author={this.state.authors[post.userId].name}
+                        key={post.id}
+                        clicked={() => this.postSelectedHandler(post.id)}
+                    />
+                );
+            });
+            if (this.state.selectedPostId) {
+                selectedPostId = this.state.selectedPostId;
+                selectedPost = this.state.postData[selectedPostId];
+                title = selectedPost.title;
+                body = selectedPost.body;
+                author = this.state.authors[selectedPost.userId].name;
+            }
+        } else {
+            posts = <p>Something went wrong!</p>;
+        }
 
         return (
             <div>
@@ -51,7 +87,13 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost id={this.state.selectedPostId} />
+                    <FullPost
+                        id={selectedPostId}
+                        title={title}
+                        author={author}
+                        body={body}
+                        deletePost={() => this.deletePostHandler(selectedPostId)}
+                    />
                 </section>
                 <section>
                     <NewPost />
